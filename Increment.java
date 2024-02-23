ab9b02fd6506eb6bd36611793781214af1661a80
HAAMIAH-2 <161025243+HAAMIAH-2@users.noreply.github.com>
2024-02-23 16:26:48 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,14 @@
+class Increment
+{
+    public static void main (String args[])
+    {
+        int num=7;
+        num=num++;
+        //System.out.println(num);
+        //num = ++num;
+        System.out.println(num);
+        num=num++;
+        System.out.println(num);
+        
+    }
+}
